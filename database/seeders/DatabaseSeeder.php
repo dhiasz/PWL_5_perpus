0a119bfc4f210e02d7686ebe278a0dcbcc62e751
dhias <dhiasa01@gmail.com>
2024-11-06 12:41:43 +0700

    maling Sudah Update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([BookshelfSeeder::class]);
+        Roleseeder::class;
+        Userseeder::class;
     }
 }
